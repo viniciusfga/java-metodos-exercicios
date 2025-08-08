45f8aba3b1f77a4273d1aef34e1468eb240f2439
Vinicius F.G. Araújo <viniciusfg.araujo@gmail.com>
2025-08-07 22:38:31 -0300

    - Primeiro commit

--- Aprendiz/src/Nivel_01/Exercicio01.java
+++ Aprendiz/src/Nivel_01/Exercicio01.java
@@ -3,11 +3,6 @@ package Nivel_01;
 public class Exercicio01 {
     public static void main(String[] args) {
 
-        // Descrição do exercício
-        System.out.println("Exercício 1: Declarar uma matriz 2x2 de inteiros.");
-
-        // Exemplo 1 - Declaração de uma matriz 2x2 vazia
-        int[][] matriz_A = new int[2][2];
 
     }
 }
