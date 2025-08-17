02ff45def4bb382a30d71778a203d3a1aa71d9b9
Vinicius F.G. Araújo <viniciusfg.araujo@gmail.com>
2025-08-17 17:37:44 -0300

    - Add Exercicio02

--- Intermediario/src/Nivel_4/Exercicio02.java
+++ Intermediario/src/Nivel_4/Exercicio02.java
@@ -3,5 +3,25 @@ package Nivel_4;
 public class Exercicio02 {
     public static void main(String[] args){
 
+        System.out.println("Criar um método que recebe um array de double e retorna a média.");
+
+        double[] vetor = {1.5 , 1.5 , 1.5};
+
+        double media = CalcularMedia(vetor);
+
+        System.out.println("Média: " + media);
+
     }
+    public static double CalcularMedia(double[] vet){
+        double soma = 0;
+
+        for (int i = 0; i < vet.length; i++) {
+            soma += vet[i];
+        }
+
+        double media = soma / vet.length;
+
+        return media;
+    }
+
 }
