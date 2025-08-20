34b02b0ab5030fe76b53ba465e7439cd42876fb2
2025101202010036 <viniciusfg.araujo@gmail.com>
2025-08-20 10:10:30 -0300

    - Add Exercicio12 fix

--- Aprendiz/src/Nivel_2/Exercicio12.java
+++ Aprendiz/src/Nivel_2/Exercicio12.java
@@ -1,11 +1,11 @@
 package Nivel_2;
 
 public class Exercicio12 {
-    public static void maim(String[] args){
+    public static void main (String[] args){
 
         System.out.println("Criar um método que retorna se um número é múltiplo de 3 ou 5.");
 
-        int numero = 10;
+        int numero = 11;
 
         boolean multiplo = VerificarMultiplo(numero);
 
@@ -16,7 +16,10 @@ public class Exercicio12 {
         }
     }
     public static boolean VerificarMultiplo (int n){
-        return n % 3 == 0 || n % 5 == 0;
+        if ( n % 3 == 0 || n % 5 == 0) {
+            return true;
+        }
+        return false;
     }
 
 }
---
+++ Aprendiz/src/Nivel_2/Exercicio13.java
@@ -0,0 +1,8 @@
+package Nivel_2;
+
+public class Exercicio13 {
+    public static void main(String[] args) {
+
+
+    }
+}
