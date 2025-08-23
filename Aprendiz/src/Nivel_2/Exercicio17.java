a3aad164af0c23f41e041b558072110e68e2f735
Vinicius F.G. Araújo <viniciusfg.araujo@gmail.com>
2025-08-23 13:27:32 -0300

    - Add Exercicio17

--- Aprendiz/src/Nivel_2/Exercicio17.java
+++ Aprendiz/src/Nivel_2/Exercicio17.java
@@ -3,9 +3,26 @@ package Nivel_2;
 public class Exercicio17 {
     public static void main(String[] args) {
 
-        System.out.println("");
+        System.out.println("Criar um método que recebe um caractere e retorna se é vogal.");
 
-
+        char letra = 'i';
+        boolean vogal = isVogal(letra);
+        if (vogal) {
+            System.out.println("É vogal");
+        } else {
+            System.out.println("Não é vogal");
+        }
 
     }
+
+    public static boolean isVogal(char l) {
+        char[] vogais = {'a', 'e', 'i', 'o', 'u'};
+        for (int i = 0; i < vogais.length; i++) {
+            if (l == vogais[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
