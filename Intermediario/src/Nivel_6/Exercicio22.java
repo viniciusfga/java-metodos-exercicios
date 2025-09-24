3323b75b50ccdf99cc1c86638b6673256e93f7a4
2025101202010036 <viniciusfg.araujo@gmail.com>
2025-09-24 09:04:52 -0300

    - Add Exercicio22 Correção

--- Intermediario/src/Nivel_6/Exercicio22.java
+++ Intermediario/src/Nivel_6/Exercicio22.java
@@ -11,9 +11,20 @@ public class Exercicio22 {
             char c = texto.charAt(i);
             char proximo;
 
-            if (c == 'Z') proximo = 'A';
-            else if (c == 'z') proximo = 'a';
-            else proximo = (char) (c + 1);
+//            if (c == 'Z') proximo = 'A';
+//            else if (c == 'z') proximo = 'a';
+//            else proximo = (char) (c + 1);
+            if (c >= 'A' && c <= 'Y') {
+                proximo = (char) (c + 1);
+            } else if (c == 'Z') {
+                proximo = 'A';
+            } else if (c >= 'a' && c <= 'y') {
+                proximo = (char) (c + 1);
+            } else if (c == 'z') {
+                proximo = 'a';
+            } else {
+                proximo = c; // números, espaços, símbolos permanecem iguais
+            }
 
             strCripto.append(proximo);
         }
@@ -23,10 +34,11 @@ public class Exercicio22 {
 
     public static void main(String[] args) {
 
-        String text = "ABCD";
+        String text = "ABCD XYZ abc xyz 123!";
 
         String cripto = criptografar(text);
         System.out.println(cripto);
+        // Saída: BCDE YZA bcd yza 123!
 
     }
 }
