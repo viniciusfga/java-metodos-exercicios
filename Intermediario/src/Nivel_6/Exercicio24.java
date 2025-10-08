01b7e10dd0a5424bd5fc99819dc6929bf713818a
viniciusfga <viniciusfg.araujo@gmail.com>
2025-10-07 23:32:03 -0300

    - Add Exercicio24

--- Intermediario/src/Nivel_6/Exercicio24.java
+++ Intermediario/src/Nivel_6/Exercicio24.java
@@ -1,10 +1,29 @@
 package Nivel_6;
 
+import java.util.Random;
+
 public class Exercicio24 {
     /*
     Criar um método que gera uma senha aleatória com letras, números e símbolos.
      */
-    public static void main(String[] args) {
 
+    static String getPassword(Random random) {
+        String alfabeto = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*";
+        int tamanho = 10;
+        StringBuilder sb = new StringBuilder(tamanho);
+
+        for (int i = 0; i < tamanho; i++) {
+            int indiceAleatorio = random.nextInt(alfabeto.length());
+            sb.append(alfabeto.charAt(indiceAleatorio));
+        }
+
+        return sb.toString();
     }
+
+    public static void main(String[] args) {
+        Random aleatorio = new Random();
+        String senha = getPassword(aleatorio);
+        System.out.println("Senha gerada: " + senha);
+    }
+
 }
---
+++ Intermediario/src/Nivel_6/Exercicio25.java
@@ -0,0 +1,11 @@
+package Nivel_6;
+
+public class Exercicio25 {
+    /*
+    Criar um método que recebe um texto e retorna um mapa
+    (Map<Character, Integer>) com a contagem de cada caractere.
+     */
+    public static void main(String[] args) {
+
+    }
+}
