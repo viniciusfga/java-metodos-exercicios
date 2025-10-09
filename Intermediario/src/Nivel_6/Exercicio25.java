325e6a32c93c7919a99af080ad3b45b93a0c3b38
viniciusfga <viniciusfg.araujo@gmail.com>
2025-10-08 21:36:36 -0300

    Add Exercicio 25 Nivel 6

--- Intermediario/src/Nivel_6/Exercicio25.java
+++ Intermediario/src/Nivel_6/Exercicio25.java
@@ -1,11 +1,42 @@
 package Nivel_6;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Exercicio25 {
     /*
     Criar um método que recebe um texto e retorna um mapa
     (Map<Character, Integer>) com a contagem de cada caractere.
      */
+
+    public static Map<Character, Integer> contarCaracteres(String texto) {
+        Map<Character, Integer> contagem = new HashMap<>();
+
+        for (char c : texto.toCharArray()) {
+            contagem.put(c, contagem.getOrDefault(c, 0) + 1);
+        }
+
+        // PARA ENTENDER MELHOR
+
+        for (int i = 0; i < texto.length(); i++) {
+            char caracter = texto.charAt(i);
+
+            if (contagem.containsKey(caracter)) {
+                int valorAtual = contagem.get(caracter);
+                contagem.put(caracter, valorAtual + 1);
+            } else  {
+                contagem.put(caracter, 1);
+            }
+        }
+
+        return contagem;
+    }
+
     public static void main(String[] args) {
 
+        String text = "vinicius";
+        Map<Character, Integer> cont = contarCaracteres(text);
+
+        System.out.println(cont);
     }
 }
