c14489488b420e8e1dcbc64a026716cbefe061c8
viniciusfga <viniciusfg.araujo@gmail.com>
2025-10-18 07:38:54 -0300

    - Add Exercicio03 Nivel 7

---
+++ Avancado/src/Nivel_7/Exercicio03.java
@@ -0,0 +1,22 @@
+package Nivel_7;
+
+public class Exercicio03 {
+    /*
+    Implementar um método que calcule a potência de um número usando recursão.
+     */
+    public static int potencia(int base, int expoente) {
+        if (expoente == 0) {
+            return 1; // Caso base
+        } else {
+            return base * potencia(base, expoente - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int numero = 10;
+        int expoente = 2;
+        int potencia = potencia(numero, expoente);
+        System.out.println(potencia);
+    }
+}
